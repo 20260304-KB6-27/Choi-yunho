dd4ff6b779616a4e8c0536e5f6521883f15bd51a
Choi-yunho <choiyh2001@naver.com>
2026-04-22 16:01:54 +0900

    0422 07-exception

--- 04_Java/ch07-exception/src/lecture/exam02/Application1.java
+++ 04_Java/ch07-exception/src/lecture/exam02/Application1.java
@@ -1,4 +1,15 @@
 package lecture.exam02;
 
 public class Application1 {
+    public static void main(String[] args) {
+
+        int inputNum = 5;
+        if (inputNum != 0 ) {
+            System.out.println("10 / 사용자입력값: " + 10 / inputNum);
+        } else {
+            System.out.println("잘못 입력하셨습니다.");
+        }
+        System.out.println("프로그램 종료");
+
+    }
 }
--- 04_Java/ch07-exception/src/lecture/exam03/Application.java
+++ 04_Java/ch07-exception/src/lecture/exam03/Application.java
@@ -1,4 +1,13 @@
 package lecture.exam03;
 
 public class Application {
+    public static void main(String[] args) {
+
+        FileService fileService = new FileService();
+
+        fileService.createFile();
+        fileService.readFile();
+
+        System.out.println("프로그램 종료");
+    }
 }
