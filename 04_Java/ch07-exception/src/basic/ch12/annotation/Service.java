a29e8c080163c710bc834a0c1f452bcd220851a4
Choi-yunho <choiyh2001@naver.com>
2026-04-23 15:56:44 +0900

    0423 ch07-exception/basic

---
+++ 04_Java/ch07-exception/src/basic/ch12/annotation/Service.java
@@ -0,0 +1,18 @@
+package basic.ch12.annotation;
+
+public class Service {
+    @PrintAnnotation  // 둘 다 기본 값
+    public void method1() {
+        System.out.println("실행 내용 1");
+
+    }
+    @PrintAnnotation("*") // value = '*', number = 기본값
+    public void method2() {
+        System.out.println("실행 내용 2");
+
+    }@PrintAnnotation(value="*", number = 5) // value = '*', number = 20
+    public void method3() {
+        System.out.println("실행 내용 3");
+
+    }
+}
