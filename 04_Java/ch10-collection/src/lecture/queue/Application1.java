56569a7a75e3f6e0f32606ccc4e515cd3b96b0af
Choi-yunho <choiyh2001@naver.com>
2026-04-28 15:46:05 +0900

    0428 ch10-collection

---
+++ 04_Java/ch10-collection/src/lecture/queue/Application1.java
@@ -0,0 +1,30 @@
+package lecture.queue;
+
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class Application1 {
+
+    public static void main(String[] args) {
+
+        /*
+        * Queue
+        *
+        * 주요 메서드
+        * - offer : 요소 추가 (실패했을 때 false 반환) (add()는 예외 발생)
+        * - peek : 가장 앞 요소 반환
+        * - poll : 가장 앞 요소 반환 후 제거
+        * */
+
+        Queue<String> queue = new LinkedList<>();
+
+        queue.offer("First");
+        queue.offer("Second");
+        queue.offer("Third");
+
+        System.out.println("queue.peek() = " + queue.peek());
+        System.out.println("queue.poll() = " + queue.poll());  // 앞 요소 꺼내고 제거
+        System.out.println("queue.peek() = " + queue.peek());
+    }
+}
---
+++ 04_Java/ch10-collection/src/lecture/queue/Application2.java
@@ -0,0 +1,36 @@
+package lecture.queue;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Application2 {
+
+    public static void main(String[] args) {
+
+        /*
+        * Deque (덱)
+        * - Queue를 확장한 인터페이스로 양 쪽 끝에서 모두 삽입 및 삭제 가능
+        *
+        * 주요 메서드
+        * - offerFirst() / offerLast()
+        * - peekFirst() / peekLast()
+        * - pollFirst() / pollLast()
+        * */
+
+        Deque<String> deque = new ArrayDeque<>();
+
+        deque.offerLast("middle");
+        deque.offerFirst("front");
+        deque.offerLast("end");
+
+        System.out.println("deque = " + deque);
+
+        System.out.println("deque.peekFirst() = " + deque.peekFirst());
+        System.out.println("deque.peekLast() = " + deque.peekLast());
+
+        System.out.println("deque.pollFirst() = " + deque.pollFirst()); // first
+        System.out.println("deque = " + deque);                         // middle end
+        System.out.println("deque.pollLast() = " + deque.pollLast());   // end
+        System.out.println("deque = " + deque);                         // middle
+    }
+}
